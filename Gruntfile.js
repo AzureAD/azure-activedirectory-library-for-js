6cc6572a703b9a4e55cbbd91f2b556e69ac87897
Omer Cansizoglu <cansizoc@gmail.com>
2014-09-29 16:44:59 -0700

    add gruntfile for builds

---
+++ Gruntfile.js
@@ -0,0 +1,45 @@
+module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        clean: ['build/'],
+        jsdoc: {
+            dist: {
+                src: ['lib/*.js'],
+                options: {
+                    destination: 'doc'
+                }
+            }
+        },
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc',
+                ignores: []
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'src/<%= pkg.name %>.js',
+                dest: 'build/<%= pkg.name %>.min.js'
+            }
+        },
+        karma: {
+            unit: {
+                configFile: 'karma.conf.js'
+            }
+        }
+    });
+
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-jsdoc');
+    grunt.loadNpmTasks('grunt-karma');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask('build', ['jshint', 'karma', 'jsdoc']);
+
+};

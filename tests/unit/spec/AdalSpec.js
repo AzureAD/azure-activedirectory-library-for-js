09fa1b73531e70803c690333fde491640bd1c1fc
Tushar Gupta <tushargupta51@gmail.com>
2016-06-24 17:58:20 -0700

    set login hint to upn only

--- tests/unit/spec/AdalSpec.js
+++ tests/unit/spec/AdalSpec.js
@@ -229,7 +229,7 @@ describe('Adal', function () {
             token = valToken;
         };
         adal._renewStates = [];
-        adal._user = { userName: 'test@testuser.com' };
+        adal._user = { profile: { 'upn': 'test@testuser.com' }, userName: 'test@domain.com'};
         adal.acquireToken(RESOURCE1, callback);
         expect(adal.callback).toBe(callback);
         expect(storageFake.getItem(adal.CONSTANTS.STORAGE.LOGIN_REQUEST)).toBe('');
@@ -266,7 +266,7 @@ describe('Adal', function () {
         };
 
         adal._renewStates = [];
-        adal._user = { userName: 'test@testuser.com' };
+        adal._user = { profile: { 'upn': 'test@testuser.com' }, userName: 'test@domain.com' };
         adal.acquireToken(RESOURCE1, callback);
         //Simulate second acquire i.e. second service call from Angular.
         adal.acquireToken(RESOURCE1, callback2);
@@ -698,7 +698,7 @@ describe('Adal', function () {
             token = valToken;
         };
         adal._renewStates = [];
-        adal._user = { userName: 'test@testuser.com' };
+        adal._user = { profile: { 'upn': 'test@testuser.com' }, userName: 'test@domain.com' };
         adal.acquireToken(adal.config.clientId, callback);
         expect(storageFake.getItem(adal.CONSTANTS.STORAGE.NONCE_IDTOKEN)).toBe('33333333-3333-4333-b333-333333333333');
         expect(adal.config.state).toBe('33333333-3333-4333-b333-333333333333' + '|' + 'client');
@@ -769,7 +769,7 @@ describe('Adal', function () {
         var callback = function () {
         };
         adal._renewStates = [];
-        adal._user = { userName: 'test@testuser.com' };
+        adal._user = { profile: { 'upn': 'test@testuser.com' }, userName: 'test@domain.com' };
         spyOn(adal, '_loadFrameTimeout');
         adal.acquireToken(RESOURCE1, callback);
         expect(adal._loadFrameTimeout).toHaveBeenCalledWith(DEFAULT_INSTANCE + conf.tenant + '/oauth2/authorize?response_type=token&client_id=client&resource=' + RESOURCE1 + '&redirect_uri=contoso_site&state=33333333-3333-4333-b333-333333333333%7Ctoken.resource1'
@@ -789,7 +789,7 @@ describe('Adal', function () {
         var callback = function () {
         };
         adal._renewStates = [];
-        adal._user = { userName: 'test@testuser.com' };
+        adal._user = { profile: { 'upn': 'test@testuser.com' }, userName: 'test@domain.com' };
         mathMock.random = function () {
             return 0.1;
         };

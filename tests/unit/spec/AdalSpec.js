e0fb87cf338901ee86b51db080bb4db505cf49bb
Rohit Narula <ronaru@microsoft.com>
2016-07-13 16:09:09 -0700

    fixed regex in token_deserialize method and added unit tests

--- tests/unit/spec/AdalSpec.js
+++ tests/unit/spec/AdalSpec.js
@@ -67,6 +67,10 @@ describe('Adal', function () {
     var SECONDS_TO_EXPIRE = 3600;
     var DEFAULT_INSTANCE = "https://login.microsoftonline.com/";
     var IDTOKEN_MOCK = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IjVUa0d0S1JrZ2FpZXpFWTJFc0xDMmdPTGpBNCJ9.eyJhdWQiOiJlOWE1YThiNi04YWY3LTQ3MTktOTgyMS0wZGVlZjI1NWY2OGUiLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLXBwZS5uZXQvNTJkNGIwNzItOTQ3MC00OWZiLTg3MjEtYmMzYTFjOTkxMmExLyIsImlhdCI6MTQxMTk1OTAwMCwibmJmIjoxNDExOTU5MDAwLCJleHAiOjE0MTE5NjI5MDAsInZlciI6IjEuMCIsInRpZCI6IjUyZDRiMDcyLTk0NzAtNDlmYi04NzIxLWJjM2ExYzk5MTJhMSIsImFtciI6WyJwd2QiXSwib2lkIjoiZmEzYzVmYTctN2Q5OC00Zjk3LWJmYzQtZGJkM2E0YTAyNDMxIiwidXBuIjoidXNlckBvYXV0aGltcGxpY2l0LmNjc2N0cC5uZXQiLCJ1bmlxdWVfbmFtZSI6InVzZXJAb2F1dGhpbXBsaWNpdC5jY3NjdHAubmV0Iiwic3ViIjoiWTdUbXhFY09IUzI0NGFHa3RjbWpicnNrdk5tU1I4WHo5XzZmbVc2NXloZyIsImZhbWlseV9uYW1lIjoiYSIsImdpdmVuX25hbWUiOiJ1c2VyIiwibm9uY2UiOiI4MGZmYTkwYS1jYjc0LTRkMGYtYTRhYy1hZTFmOTNlMzJmZTAiLCJwd2RfZXhwIjoiNTc3OTkxMCIsInB3ZF91cmwiOiJodHRwczovL3BvcnRhbC5taWNyb3NvZnRvbmxpbmUuY29tL0NoYW5nZVBhc3N3b3JkLmFzcHgifQ.WHsl8TH1rQ3dQbRkV0TS6GBVAxzNOpG3nGG6mpEBCwAOCbyW6qRsSoo4qq8I5IGyerDf2cvcS-zzatHEROpRC9dcpwkRm6ta5dFZuouFyZ_QiYVKSMwfzEC_FI-6p7eT8gY6FbV51bp-Ah_WKJqEmaXv-lqjIpgsMGeWDgZRlB9cPODXosBq-PEk0q27Be-_A-KefQacJuWTX2eEhECLyuAu-ETVJb7s19jQrs_LJXz_ISib4DdTKPa7XTBDJlVGdCI18ctB67XwGmGi8MevkeKqFI8dkykTxeJ0MXMmEQbE6Fw-gxmP7uJYbZ61Jqwsw24zMDMeXatk2VWMBPCuhA';
+    var STATE = '585bd348-4d52-4689-b9c7-d8480564f368';
+    var SESSION_STATE = '451c6916-27cf-4eae-81cd-accf96126398';
+    var VALID_URLFRAGMENT = 'id_token=' + IDTOKEN_MOCK + '' + '&state=' + STATE + '&session_state=' + SESSION_STATE;
+    var INVALID_URLFRAGMENT = 'id_token' + IDTOKEN_MOCK + '' + '&state=' + STATE + '&session_state=' + SESSION_STATE;
     var storageFake = function () {
         var store = {};
         return {
@@ -808,6 +812,43 @@ describe('Adal', function () {
         expect(adal._loadFrameTimeout).toHaveBeenCalledWith(DEFAULT_INSTANCE + conf.tenant + '/oauth2/authorize?response_type=token&client_id=client&resource=' + RESOURCE1 + '&redirect_uri=contoso_site&state=44444444-4444-4444-8444-444444444444%7Ctoken.resource1'
                 + '&client-request-id=44444444-4444-4444-8444-444444444444' + adal._addLibMetadata() + '&prompt=none', 'adalRenewFrametoken.resource1', 'token.resource1');
 
+    });
+
+    it('checks the deserialize method for extracting idToken', function () {
+        var obj = adal._deserialize(VALID_URLFRAGMENT);
+        expect(obj.id_token).toBe(IDTOKEN_MOCK);
+        expect(obj.state).toBe(STATE);
+        expect(obj.session_state).toBe(SESSION_STATE);
+
+        obj = adal._deserialize(INVALID_URLFRAGMENT);
+        expect(obj.id_token).toBeUndefined;
+        expect(obj.state).toBe(STATE);
+        expect(obj.session_state).toBe(SESSION_STATE);
+        expect(obj['id_token' + IDTOKEN_MOCK]).toBeUndefined;
+        var deserialize = adal._deserialize;//save initial state of function
+
+        adal._deserialize = function (query) {
+           var match,
+           pl = /\+/g,  // Regex for replacing addition symbol with a space
+           search = /([^&=]+)=?([^&]*)/g,
+           decode = function (s) {
+               return decodeURIComponent(s.replace(pl, ' '));
+           },
+           obj = {};
+           match = search.exec(query);
+           while (match) {
+                obj[decode(match[1])] = decode(match[2]);
+                match = search.exec(query);
+           }
+
+           return obj;
+        }
+        obj = adal._deserialize(INVALID_URLFRAGMENT);
+        expect(obj['id_token' + IDTOKEN_MOCK]).toBe('');//This additional property is parsed because of ? operator in regex
+        expect(obj.id_token).toBeUndefined;
+        expect(obj.state).toBe(STATE);
+        expect(obj.session_state).toBe(SESSION_STATE);
+        adal._deserialize = deserialize;//reassign state to original function
     });
     // TODO angular intercepptor
     // TODO angular authenticationService

5c8045445e1be959c431864e01f5d585115712b2
Tushar Gupta <tushargupta51@gmail.com>
2016-11-08 12:07:48 -0800

    adding more tests

--- tests/e2eTestsSpec.js
+++ tests/e2eTestsSpec.js
@@ -3,7 +3,7 @@
 /* global it */
 /* global describe */
 
-describe('TaskCtl', function () {
+describe('E2ETests', function () {
     var appRootUrl, appToGoListUrl, appHomeUrl, appTodoListUrl, username, password, appSetupUrl, unassignedUsername, unassignedPassword;
     beforeEach(function () {
         appRootUrl = 'http://localhost:44332/'; //'https://adaljstestapp.azurewebsites.net/'; //'https://adaljsnonangularapp.azurewebsites.net/'; //'http://localhost:44332/';
@@ -22,43 +22,60 @@ describe('TaskCtl', function () {
     });
 
     afterEach(function () {
-        element(by.id('logoutButton')).click().then(function () {
-            browser.sleep(5000);
-            browser.getCurrentUrl().then(function (url) {
-                expect(url).toContain(appHomeUrl);
-            })
-        });
+
     });
 
-    it('tests that response ', function () {
+    it('tests login button ', function () {
         browser.ignoreSynchronization = true;
         element(by.id('loginButton')).click().then(function () {
             element(by.id('cred_userid_inputtext')).sendKeys(username);
             element(by.id('cred_password_inputtext')).sendKeys(password);
-            browser.sleep(5000);
+            browser.sleep(3000);
             element(by.id('cred_sign_in_button')).click().then(function () {
                 browser.getCurrentUrl().then(function (url) {
                     expect(url).toContain(appHomeUrl);
+                    expect(browser.executeScript(function() {
+                        return window.sessionStorage.getItem('adal.idtoken');
+                    })).not.toBe(null);
                 });
             });
         });
+        if (element(by.id('logoutButton')).isDisplayed()) {
+            element(by.id('logoutButton')).click().then(function () {
+                browser.sleep(3000);
+                browser.getCurrentUrl().then(function (url) {
+                    expect(url).toContain(appHomeUrl);
+                })
+            });
+        }
     });
 
-    it('tests that response has valid id token', function () {
+    it('tests that navigating to protected route triggers login', function () {
         browser.ignoreSynchronization = true;
         element(by.id('todoListOption')).click().then(function () {
             if (element(by.id('victor_tushartest_onmicrosoft_com_link'))) {
                 element(by.id('victor_tushartest_onmicrosoft_com_link')).click().then(function () {
                     element(by.id('cred_password_inputtext')).sendKeys(password);
-                    browser.sleep(5000);
+                    browser.sleep(3000);
                     element(by.id('cred_sign_in_button')).click().then(function () {
                         browser.getCurrentUrl().then(function (url) {
-                            expect(url).toContain(appTodoListUrl);
+                            expect(url).toContain(appHomeUrl);
+                            expect(browser.executeScript(function () {
+                                return window.sessionStorage.getItem('adal.idtoken');
+                            })).not.toBe(null);
                         });
                     });
                 });
             }
         });
+        if (element(by.id('logoutButton')).isDisplayed()) {
+            element(by.id('logoutButton')).click().then(function () {
+                browser.sleep(3000);
+                browser.getCurrentUrl().then(function (url) {
+                    expect(url).toContain(appHomeUrl);
+                })
+            });
+        }
     });
 
     it('tests login with an unassigned user ', function () {
@@ -68,10 +85,22 @@ describe('TaskCtl', function () {
                 element(by.id('use_another_account_link')).click().then(function () {
                     element(by.id('cred_userid_inputtext')).sendKeys(unassignedUsername);
                     element(by.id('cred_password_inputtext')).sendKeys(unassignedPassword);
-                    browser.sleep(5000);
+                    browser.sleep(3000);
                     element(by.id('cred_sign_in_button')).click().then(function () {
                         browser.getCurrentUrl().then(function (url) {
                             expect(url).toContain(appHomeUrl);
+                            expect(element(by.id('logoutButton')).isDisplayed()).toBe(false);
+                            browser.executeScript(function () {
+                                return 
+                                {
+                                    'error': window.sessionStorage.getItem('adal.error'),
+                                    'idtoken': window.sessionStorage.getItem('adal.idtoken')
+                                };
+                            }).then(function (storage) {
+                                console.log(storage);
+                                expect(storage.error).toBe('access_denied');
+                                expect(storage.idtoken).toBe(null);
+                            });
                         });
                     });
                 });

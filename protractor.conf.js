86ddd65ea3c634d43bb5c304065d1905ecb8b337
Tushar Gupta <tushargupta51@gmail.com>
2017-01-31 18:23:17 -0800

    Add end to end tests and update protractor configs

--- protractor.conf.js
+++
@@ -1,9 +0,0 @@
-exports.config = {
-  framework: 'jasmine',
-  seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['tests/e2eTestsSpec.js'],
-  capabilities: {
-    browserName: 'firefox'
-  },
-  directConnect: true
-}
---
+++ tests/endToEndTests/testRunner.js
@@ -0,0 +1,29 @@
+var exec = require('child_process').exec;
+var fs = require('fs');
+
+var configurationFiles = [
+    "protractor.conf.chrome.js",
+    "protractor.conf.firefox.js",
+    "protractor.conf.ie.js",
+    "protractor.conf.safari.js",
+    "protractor.conf.androidNexus5.js",
+    "protractor.conf.edge.js",
+];
+
+var i = 0;
+
+var runProtractorConfiguration = function () {
+    console.log("Running " + configurationFiles[i]);
+    var testCommand = 'node runTests.js ' + configurationFiles[i];
+    exec(testCommand,
+        function (error, stdout, stderr) {
+            console.log("Completed " + configurationFiles[i]);
+            fs.writeFileSync("output-" + configurationFiles[i], stdout);
+            i++;
+            if (i < configurationFiles.length) {
+                runProtractorConfiguration();
+            }
+        });
+}
+
+runProtractorConfiguration();

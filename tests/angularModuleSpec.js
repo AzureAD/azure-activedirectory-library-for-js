6d3af3a45aca28ce5f02bb4742f7ff7fbbbe0b42
Rohit Narula <ronaru@microsoft.com>
2017-02-14 14:23:50 -0800

    update node version for travis.yml

--- tests/angularModuleSpec.js
+++ tests/angularModuleSpec.js
@@ -488,7 +488,7 @@ describe('AcquireTokenCtl', function () {
         store = {
             'adal.token.keys': adalServiceProvider.config.loginResource + '|',
             'adal.access.token.keyloginResource123': token,
-            'adal.expiration.keyloginResource123': 122
+            'adal.expiration.keyloginResource123': 302
         };
         scope.$on('adal:acquireTokenSuccess', function (event, message) {
             tokenOut = message;
